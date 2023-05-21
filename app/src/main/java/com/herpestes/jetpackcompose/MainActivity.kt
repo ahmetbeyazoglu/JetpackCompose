a8be435b8d9d31ed812c7ff0d3cc4df44e469950
herpestes <ahmetbeyazoglu@gmail.com>
2023-05-21 16:04:02 +0300

    Compose fix

--- app/src/main/java/com/herpestes/jetpackcompose/MainActivity.kt
+++ app/src/main/java/com/herpestes/jetpackcompose/MainActivity.kt
@@ -16,18 +16,20 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            JetPackComposeTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
+            Greeting(name = "Android")
+
             }
+        testFunction(5,::testFunctionLambda)
         }
+    fun testFunction(int: Int, myfunction : () -> Unit){
+
     }
-}
+    fun testFunctionLambda (){
+        println("test")
+    }
+
+    }
+
 
 @Composable
 fun Greeting(name: String) {
